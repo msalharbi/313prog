26c1c2f4620bcb9bccc13fc2a08ced7e69b3052c
msalharbi <msalharbi@gmail.com>
2025-03-10 22:40:36 +0300

    Update Shape.java

--- methods/2/Shape.java
+++ methods/2/Shape.java
@@ -1,1 +1,14 @@
+package myfirstapp;
 
+public class Shape {
+    // properties
+    String name;
+    
+    void setName(String n){
+        name = n;
+    }
+    
+    String getName(){
+        return name;
+    }
+}
