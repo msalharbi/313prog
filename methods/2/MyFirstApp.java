e8591456c5ac15da790d26f5ba753a84d17f7648
msalharbi <msalharbi@gmail.com>
2025-03-10 22:40:14 +0300

    Update MyFirstApp.java

--- methods/2/MyFirstApp.java
+++ methods/2/MyFirstApp.java
@@ -1,1 +1,16 @@
 
+package myfirstapp;
+
+public class MyFirstApp {
+
+    public static void main(String[] args) {
+
+        Shape s = new Shape();
+        s.setName("circle");
+        
+        System.out.println(s.getName());
+        
+        //square
+        
+    }
+}
